b30558c226dc16e7806a0f041eedd46788b22d94
hilarysoliveira <126517494+hilarysoliveira@users.noreply.github.com>
2023-09-04 14:17:08 -0400

    compra

--- Telas/Compra.xaml.cs
+++ Telas/Compra.xaml.cs
@@ -1,27 +1,70 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
-namespace NewAppCacauShow.Telas
+namespace SistemaDeCompra
 {
-    /// <summary>
-    /// Lógica interna para Compra.xaml
-    /// </summary>
-    public partial class Compra : Window
+    public partial class MainWindow : Window
     {
-        public Compra()
+        private ObservableCollection<ItemCompra> carrinho = new ObservableCollection<ItemCompra>();
+
+        public MainWindow()
         {
             InitializeComponent();
+            lvCarrinho.ItemsSource = carrinho;
         }
+
+        private void AdicionarAoCarrinho_Click(object sender, RoutedEventArgs e)
+        {
+            string produto = txtProduto.Text;
+            if (string.IsNullOrWhiteSpace(produto))
+            {
+                MessageBox.Show("Por favor, insira um nome de produto válido.");
+                return;
+            }
+
+            if (!int.TryParse(txtQuantidade.Text, out int quantidade) || quantidade <= 0)
+            {
+                MessageBox.Show("Por favor, insira uma quantidade válida.");
+                return;
+            }
+
+            if (!decimal.TryParse(txtPrecoUnitario.Text, out decimal precoUnitario) || precoUnitario <= 0)
+            {
+                MessageBox.Show("Por favor, insira um preço unitário válido.");
+                return;
+            }
+
+            decimal subtotal = quantidade * precoUnitario;
+            carrinho.Add(new ItemCompra { Produto = produto, Quantidade = quantidade, PrecoUnitario = precoUnitario, Subtotal = subtotal });
+            CalcularTotal();
+            LimparCampos();
+        }
+
+        private void CalcularTotal()
+        {
+            decimal total = 0;
+            foreach (var item in carrinho)
+            {
+                total += item.Subtotal;
+            }
+            lblTotal.Content = $"Total: R$ {total:F2}";
+        }
+
+        private void LimparCampos()
+        {
+            txtProduto.Clear();
+            txtQuantidade.Clear();
+            txtPrecoUnitario.Clear();
+        }
+    }
+
+    public class ItemCompra
+    {
+        public string Produto { get; set; }
+        public int Quantidade { get; set; }
+        public decimal PrecoUnitario { get; set; }
+        public decimal Subtotal { get; set; }
     }
 }
