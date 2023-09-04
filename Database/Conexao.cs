f8f4d196e51e88ea8cde97f9d4b9e5334f9c68d9
Thauany Celestino <83665046+ThauanyCelestino@users.noreply.github.com>
2023-09-04 14:06:42 -0400

    Iniciando conexão com banco de dados/ Database

---
+++ Database/Conexao.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace NewAppCacauShow.Database
+{
+    internal class Conexao
+    {
+        private static string host = "localhost";
+
+        private static string port = "3308";
+
+        private static string user = "root";
+
+        private static string password = "root";
+
+        private static string dbname = "dssistemas_db";
+
+        private static MySqlConnection connection;
+
+        private static MySqlCommand command;
+
+        public Conexao()
+        {
+            try
+            {
+                connection = new MySqlConnection($"server={host};database={dbname};port={port};user={user};password={password}");
+                connection.Open();
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public MySqlCommand Query()
+        {
+            try
+            {
+                command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+
+                return command;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+
+    }
+}
