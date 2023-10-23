bfc84363c13499717a2c9c6d5a5e3abbae31de94
Hilary <hilarysoliveira@gmail.com>
2023-10-23 16:26:03 -0300

    DAO e CS

---
+++ Classes/ProdutoCadastrarDAO.cs
@@ -0,0 +1,93 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewAppCacauShow.Classes
+{
+    internal class ProdutoCadastrarDAO
+    {
+        
+
+        public static Conexao conn;
+
+        public ProdutoCadastrarDAO() 
+        { 
+            conn = new Conexao();
+        }
+        public ProdutoCadastrar GetById(int id)
+        {
+            var query = conn.Query();
+            query.CommandText = "SELECT * ProdutoCadastrar WHERE id_for = @id";
+
+            query.Parameters.AddWithValue("id", id);
+
+            MySqlDataReader reader= query.ExecuteReader();
+            var produto = new ProdutoCadastrar();
+
+            if(reader.Read())
+            {
+                produto.Id = reader.GetInt32("id_pro");
+                produto.Nome = reader.GetString("nomer_pro");
+                produto.Codigo = reader.GetString("cod_pro");
+                produto.Vencimento = reader.GetDateTime("venc_pro");
+                produto.ValorCompra = reader.GetFloat("com_pro");
+                produto.ValorVenda = reader.GetFloat("ven_pro");
+                produto.Descricao = reader.GetString("des_pro-)");
+            }
+            return produto;
+        }
+        public List<ProdutoCadastrar> List() 
+        {
+            try
+            {
+                List<ProdutoCadastrar> list = new List<ProdutoCadastrar>();
+
+                var query = conn.Query();
+                query.CommandText = "SELECT * FROM Fornecedor";
+
+                MySqlDataReader reader = query.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new ProdutoCadastrar()
+                    {
+                        Id = reader.GetInt32("id"),
+                        Nome = reader.GetString("id_pro"),
+                        Codigo = reader.GetString("cod_pro"),
+                        Vencimento = reader.GetDateTime("venc_pro"),
+                        ValorCompra = reader.GetFloat("com_pro"),
+                        ValorVenda = reader.GetFloat("ven_pro")
+                    });
+                }
+                return list;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            { 
+                conn.Close();
+            }
+        }
+
+        public void Delete(ProdutoCadastrar produtoCadastrar)
+        {
+            try 
+            { 
+                var query = conn.Query();
+                query.CommandText= "Delete";
+
+            } catch(Exception ex)
+            {
+
+            }
+        }
+        }
+
+         
+    }
+
